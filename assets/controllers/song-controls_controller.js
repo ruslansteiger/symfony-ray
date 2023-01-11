871b1f80ec35989607e53926e17d59e9fccbf1b0
Ruslan Steiger <steiger.ruslan@gmail.com>
2023-01-11 21:26:35 +0100

    wip

--- assets/controllers/song-controls_controller.js
+++ assets/controllers/song-controls_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from '@hotwired/stimulus';
+import axios from 'axios'
 
 /*
  * This is an example Stimulus controller!
@@ -10,7 +11,18 @@ import { Controller } from '@hotwired/stimulus';
  * Delete this file or adapt it for your use!
  */
 export default class extends Controller {
-    connect() {
-        this.element.textContent = 'Hello Stimulus! Edit me in assets/controllers/hello_controller.js';
+    static values = {
+        infoUrl: String,
+    }
+
+    play(event) {
+        event.preventDefault()
+
+        axios.get(this.infoUrlValue)
+            .then(response => {
+                let audio = new Audio(response.data.url)
+
+                audio.play()
+            })
     }
 }
--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -3,13 +3,13 @@
 namespace App\Controller;
 
 use Psr\Log\LoggerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SongController extends AbstractController
 {
-    #[Route('api/songs/{id<\d+>}', methods: ['GET'])]
+    #[Route('api/songs/{id<\d+>}', name: 'api_song_show', methods: ['GET'])]
     public function getSong(int $id, LoggerInterface $log): Response
     {
         $song = [
