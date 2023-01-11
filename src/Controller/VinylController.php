8a75c67f3fcdca961bc3c4a43f62a98ca453fd3b
Ruslan Steiger <steiger.ruslan@gmail.com>
2023-01-11 16:25:26 +0100

    wip

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
 
 class VinylController
 {
@@ -11,5 +12,17 @@ class VinylController
     public function homepage(): Response
     {
         return new Response('🚀 Ray demonstration in a symfony application.');
+    }
+
+    #[Route('/browse/{slug}')]
+    public function browse(?string $slug = null): Response
+    {
+        if ($slug) {
+            $title = 'Genre: '.u(str_replace('-', ' ', $slug))->title(true);
+        } else {
+            $title = 'All genres!';
+        }
+
+        return new Response($title);
     }
 }
